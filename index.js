4b4c392eaebba2fcf73596c99c21695a7ef34cd0
BradleyDeveloppeur <julienbradleycold08@gmail.com>
2023-10-08 15:54:29 -0300

    Closure

---
+++ index.js
@@ -0,0 +1,41 @@
+/*function greeting(greet){
+
+    return function (name){
+        console.log(`${greet} ${name}`)
+
+    }
+}
+
+let greetingSon = greeting("Hi")
+    greetingSon("Bradley")
+*/
+
+/*const countNumber = function(){
+    let count = 0;
+
+    return function(){
+        count ++
+        console.log(`${count} number`)
+    }
+}
+
+let counter = countNumber();
+counter();
+counter();
+counter();
+*/
+
+
+var createFunction = function(){
+    let array = []
+
+    for (let i = 0; i < 3; i++){
+        array.push(() => (console.log(i)))
+    }
+    return array;
+}
+
+let arr = createFunction();
+arr[0]();
+arr[1]();
+arr[2]();
